a6ec590a937eb879b3982c079e3be3d344743e75
nruffin95 <nruffin95@gmail.com>
2022-07-17 13:11:21 -0500

    Added models folder

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    // Must match a valid email address (look into Mongoose's matching validation)
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// retrieves the length of the user's friends array field on query.
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce(
+    (total, friends) => total + friends.replies.length + 1,
+    0
+  );
+});
+
+// create the Pizza model using the PizzaSchema
+const User = model('User', UserSchema);
+
+// export the Pizza model
+module.exports = User;
